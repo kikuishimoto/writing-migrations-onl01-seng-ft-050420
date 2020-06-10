4250d86c09c3b285d95320f202f6db51c8e803dc
kiku ishimoto <kikuish@gmail.com>
2020-06-10 01:55:40 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,6 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
-  def change 
+  def change
     add_column :students, :grade, :integer
     add_column :students, :birthdate, :string
-  end 
-end 
+  end
+end
